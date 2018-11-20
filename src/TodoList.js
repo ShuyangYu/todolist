db9e1f68ab70edf7d5ac1d3d16225f6849259ea5
ShuyangYu <yushuyanggyx@163.com>
2018-11-20 21:41:05 +0800

    init

---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div><input /> <button>submit</button></div>
+        )
+    }
+}
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import TodoList from './TodoList';
+
+// pwa 访问网页后断网可访问之前的页面
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// jsx 语法, 需要引入 react
+ReactDOM.render(<TodoList />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
