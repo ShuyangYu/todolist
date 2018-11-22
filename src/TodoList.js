9c3a8c0f5ef8482d984699d6f2d56df2e87cb7aa
ShuyangYu <yushuyanggyx@163.com>
2018-11-22 15:01:58 +0800

    Update TodoList.js
    todolist输入框改变

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,36 @@
 import React, { Component, Fragment } from 'react'
 
 class TodoList extends Component {
+
+    constructor(props) {
+        super(props);//由于使用了extends，这里的super代表父类
+        this.state = {
+            inputValue : '',
+            list : []
+        }//数据要定义在状态里
+    }
     render() {
         return (
+            //定义inputValue之后输入框中的内容不再变化
             <Fragment>
-                <div><input /> <button>submit</button></div>
+                <div>
+                    <input 
+                        value={this.state.inputValue} 
+                        onChange={this.handleInputChange.bind(this)}
+                    /> 
+                    <button>submit</button>
+                </div>
                 <ul>
                     <li>learn</li>
                     <li>pratice</li>
                 </ul>
             </Fragment>
         )
+    }
+    handleInputChange(e) {
+        this.setState({
+            inputValue : e.target.value
+        })
     }
 }
 
