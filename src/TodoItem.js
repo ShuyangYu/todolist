e5d6e1f336c9d5023e5c64ac0285f5f5e93b22c8
ShuyangYu <yushuyanggyx@163.com>
2018-11-24 13:42:06 +0800

    代码性能优化

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,19 +3,21 @@ import React, { Component } from 'react'
 class TodoItem extends Component {
 
     constructor(props) {
-        super(props)
-        this.handleClick= this.handleClick.bind(this)//性能有优化
+        super(props);
+        this.handleClick= this.handleClick.bind(this); //性能有优化
     }
     render() {
+        const { content } = this.props;
         return (
-            <div onClick={this.handleClick}>
-                {this.props.content}
+            <div onClick={this.handleClick} >
+                {content}
             </div>
         )
     }
 
     handleClick() {
-        this.props.handleItemDelete(this.props.index)
+        const { handleItemDelete, index } = this.props;
+        handleItemDelete(index);
     }
 }
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import TodoItem from './TodoItem';
 import './style.css';
+
+
 class TodoList extends Component {
 
     constructor(props) {
@@ -9,6 +11,11 @@ class TodoList extends Component {
             inputValue : '',
             list : [],
         }//数据要定义在状态里
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this)
+
     }
     render() {
         return (
@@ -20,58 +27,74 @@ class TodoList extends Component {
                         id='insertArea'
                         className='input'
                         value={this.state.inputValue} 
-                        onChange={this.handleInputChange.bind(this)}
+                        onChange={this.handleInputChange}
                     /> 
-                    <button onClick={this.handleBtnClick.bind(this)}>submit</button>
+                    <button onClick={this.handleBtnClick}>submit</button>
                 </div>
                 <ul>
-                    {
-                        this.state.list.map((item, index) => {
-                            return (
-                                <div 
-                                    key={index}
-                                >
-                                    {/*  <li 
-                                        key={index} 
-                                        // 循环渲染时，需要给每一项增加key值，但是不太好
-                                        onClick={this.handleItemDelete.bind(this, index)}
-                                        dangerouslySetInnerHTML={{__html: item}} // 可以在提交框内输入html标签
-                                    >
-                                    { {item} }
-                                    </li> */}
-                                    <TodoItem 
-                                        content={item}  
-                                        index={index}
-                                        handleItemDelete={this.handleItemDelete.bind(this)} 
-                                    />{/* 父组件将item利用属性的方式传递给子组件，名称为item， 父组件将handleItemDelete这个方法利用属性的方式传递给子组件，使子组件能够调用父组件的方法*/ }
-                                    {/* this.handleItemDelete是父组件的方法，因此子组件在调用时需要绑定this*/ }
-                                </div>
-                            )
-                        })
-                    }
+                    { this.getTodoItem() }
                 </ul>
             </Fragment>
         )
     }
-    handleInputChange(e) {
-        this.setState({
-            inputValue : e.target.value
+
+    getTodoItem() {
+        return this.state.list.map((item, index) => {
+            return (
+                <div 
+                    key={index}
+                >
+                    {/*  <li 
+                        key={index} 
+                        // 循环渲染时，需要给每一项增加key值，但是不太好
+                        onClick={this.handleItemDelete.bind(this, index)}
+                        dangerouslySetInnerHTML={{__html: item}} // 可以在提交框内输入html标签
+                    >
+                    { {item} }
+                    </li> */}
+                    <TodoItem 
+                        content={item}  
+                        index={index}
+                        handleItemDelete={this.handleItemDelete} 
+                    />{/* 父组件将item利用属性的方式传递给子组件，名称为item， 父组件将handleItemDelete这个方法利用属性的方式传递给子组件，使子组件能够调用父组件的方法*/ }
+                    {/* this.handleItemDelete是父组件的方法，因此子组件在调用时需要绑定this*/ }
+                </div>
+            )
         })
     }
+
+    handleInputChange(e) {
+        // 新版react支持写法, 变成异步，提升性能
+        const value = e.target.value;
+        this.setState(() => ({
+            inputValue : value
+        }))
+        // this.setState({
+        //     inputValue : e.target.value
+        // })
+    }
     handleBtnClick() {
-        this.setState({
-            list: [...this.state.list, this.state.inputValue],
+        // setState 可接受参数 prevState, 存放修改数据之前的数据
+        this.setState((prevState) => ({
+            list: [...prevState.list, prevState.inputValue],
             inputValue: ''
-        })
+        }))
+        // this.setState({
+        //     list: [...this.state.list, this.state.inputValue],
+        //     inputValue: ''
+        // })
     }
     handleItemDelete(index) {
         // immutable state 不允许任何改变
-        const list = [...this.state.list];
-        list.splice(index, 1);
 
-        this.setState({
-            list: list
-        })
+        this.setState((prevState) => {
+            const list = [...prevState.list];
+            list.splice(index, 1);
+            return {list} //es6
+        });
+        // this.setState({
+        //     list: list
+        // })
         }
 }
 
