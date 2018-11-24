d0435851f93162f5baae8bf49e1d2975bdc49cae
ShuyangYu <yushuyanggyx@163.com>
2018-11-24 14:20:38 +0800

    PropTypes 与 DefaultTypes

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'; // 对父组件传递的props类型做强校验，函数或值
+
 
 class TodoItem extends Component {
 
@@ -19,5 +21,17 @@ class TodoItem extends Component {
         const { handleItemDelete, index } = this.props;
         handleItemDelete(index);
     }
+}
+
+
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired, // 父组件没有向子组件传递该值，如果不加isRequired就不会去检验，有时父组件无法传递，就设置默认值
+    content: PropTypes.string,
+    handleItemDelete: PropTypes.func,
+    index: PropTypes.number
+}
+
+TodoItem.defaultProps = {
+    test: 'default string',
 }
 export default TodoItem;
