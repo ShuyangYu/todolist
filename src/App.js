045affdd1eef39fe9e491e13fd6de6faaee59f35
ShuyangYu <yushuyanggyx@163.com>
2018-11-25 16:09:04 +0800

    transition-group

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './style.css';
 
 
@@ -15,7 +16,17 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <div className={this.state.show ? 'show' : 'hide'}>Hello</div>
+        {/* <div className={this.state.show ? 'show' : 'hide'}>Hello</div> */}
+        <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames='fade'
+          unmountOnExit
+          onEntered={(el) => {el.style.color='blue'}}
+          appear={true}
+        >{/* 可以实现dom的unmount*/}
+          <div>hello</div>
+        </CSSTransition>
         <button onClick={this.handleBtnClick}>toggole</button>
       </Fragment>
     )
