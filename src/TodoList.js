3f8efceae48b17c106173346182e91d3e18c3b42
ShuyangYu <yushuyanggyx@163.com>
2018-11-26 20:39:22 +0800

    使用redux中的store

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,32 +4,26 @@ import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
 import './style.css';
 import axios from 'axios'
+import store from './store'
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
 
 class TodoList extends Component {
     constructor(props) {
-        super(props)
-
+        super(props);
+        this.state = store.getState();
     }
 
     render() {
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
-                <Input placeholder='todo info' style={{width: '300px', marginRight: '10px'}}/>
+                <Input placeholder='todo info' value={this.state.inputValue} style={{width: '300px', marginRight: '10px'}}/>
                 <Button type='primary'>submit</Button>
 
                     <List
                         style={{marginTop: '10px', width: '300px'}}
                         size="small"
                         bordered
-                        dataSource={data}
+                        dataSource={this.state.list}
                         renderItem={item => (<List.Item>{item}</List.Item>)}
                     />
                 <div>hello</div>
---
+++ src/store/reducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+    inputValue: 'your to do list',
+    list: ['learn', 'pratice']
+} // 默认存储为空
+
+export default (state = defaultState, action) => {
+    return state;
+}
+// 
